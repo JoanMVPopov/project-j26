c697d6bbc0ab91382bbea7281a9ebefa549b3a4d
Yoan Popov <Y.Popov@student.tudelft.nl>
2026-05-11 16:51:55 +0200

    Misc: add the exception detector from the docs

--- src/integrationTest/kotlin/com/github/joanmvpopov/aicommitmsg/PluginIntegrationTest.kt
+++ src/integrationTest/kotlin/com/github/joanmvpopov/aicommitmsg/PluginIntegrationTest.kt
@@ -2,6 +2,9 @@ package com.github.joanmvpopov.aicommitmsg
 
 import com.intellij.driver.sdk.ui.components.common.ideFrame
 import com.intellij.driver.sdk.waitForIndicators
+import com.intellij.ide.starter.ci.CIServer
+import com.intellij.ide.starter.ci.NoCIServer
+import com.intellij.ide.starter.di.di
 import com.intellij.ide.starter.driver.engine.runIdeWithDriver
 import com.intellij.ide.starter.junit5.hyphenateWithClass
 import com.intellij.ide.starter.models.IdeInfo
@@ -11,10 +14,32 @@ import com.intellij.ide.starter.project.GitHubProject
 import com.intellij.ide.starter.runner.CurrentTestMethod
 import com.intellij.ide.starter.runner.Starter
 import org.junit.jupiter.api.Test
+import org.kodein.di.DI
+import org.kodein.di.bindSingleton
 import java.nio.file.Path
 import kotlin.time.Duration.Companion.minutes
+import org.junit.jupiter.api.fail
 
 class PluginIntegrationTest {
+
+    // source: https://plugins.jetbrains.com/docs/intellij/integration-tests-intro.html#catching-exceptions-from-ide
+    init {
+        di = DI {
+            extend(di)
+            bindSingleton<CIServer>(overrides = true) {
+                object : CIServer by NoCIServer {
+                    override fun reportTestFailure(
+                        testName: String,
+                        message: String,
+                        details: String,
+                        linkToLogs: String?
+                    ) {
+                        fail("$testName fails: $message.\n$details")
+                    }
+                }
+            }
+        }
+    }
 
     @Test
     fun pluginLoadsWithoutErrors() {
